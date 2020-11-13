818907a5b0b93560ba1a325f76d3af0c6570e63c
Rob Thornton <rthornton128@gmail.com>
2020-11-13 11:37:54 -0500

    Basic webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+// config contains general configuration for all build modes
+var config = {
+    context: path.resolve(__dirname),
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                },
+            },
+        ],
+    },
+}
+
+// by returning a function, configuration can be modified based on the build
+// mode
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        config.devtool = "eval-source-map";
+    }
+
+    return config;
+}
