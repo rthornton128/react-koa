0af64d1afc9ba2d9b29a2131024d6ce9aca93c4b
Rob Thornton <rthornton128@gmail.com>
2020-11-14 13:13:59 -0500

    Code style (#8)
    * Add editor config
    
    * Add prettier package
    
    * Ignore prettying certain files
    
    * Don't prettier github directory
    
    * Run prettier on all files
    
    * Basic prettier configuration
    
    * Add eslint packages
    
    * Configure eslint

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 A playplace to try out serving a React app with Koa
 
 ## Decisions
+
 - Yarn: Chose to use version 1 due to familiarity
-- JSX tranformation: Babel; I don't want to complicate this with additional 
-Typescript configuration 
+- JSX tranformation: Babel; I don't want to complicate this with additional
+  Typescript configuration
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const Koa = require('koa');
-const compress = require('koa-compress');
-const serve = require('koa-static');
-const path = require('path');
+const Koa = require("koa");
+const compress = require("koa-compress");
+const serve = require("koa-static");
+const path = require("path");
 
 const app = new Koa();
 
@@ -15,4 +15,4 @@ app.use(serve(path.resolve(__dirname, "../dist")));
 
 app.listen(3000);
 
-console.log("listening on port 3000")
+console.log("listening on port 3000");
