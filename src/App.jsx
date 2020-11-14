fd56e25d78d87c3c843a10d00c5ba3874c32367b
Rob Thornton <rthornton128@gmail.com>
2020-11-14 17:14:17 -0500

    Reduce complexity of App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import { Header } from "./foundation/Header.jsx";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export function App() {
-  return (
-    <div id="app" className="container">
-      <div className="row">
-        <Header />
-      </div>
-    </div>
-  );
+  return <Header />;
 }
