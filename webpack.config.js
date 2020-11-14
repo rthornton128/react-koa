1186a9e26acd7b85bed0de2539e8aa61b1967bff
Rob Thornton <rthornton128@gmail.com>
2020-11-13 20:21:34 -0500

    Testing (#7)
    * Move babel configuration into .babelrc
    
    Jest doesn't play happily with webpack. It doesn't know how to
    convert JSX to JS on its own and doesn't know how to use Webpack's
    configuration for babel. As such, the configuration for babel presets
    must be moved to an explicit .babelrc.
    
    * Enable Jest
    
    - Configure the test script to use Jest
    - Add the react-testing library and jest.
    - Configure Jest to mock stylesheets
    - Add a stylesheet mock
    
    * Test the core App entrypoint
    
    * Run tests in CI
    
    * Update workflow

--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,6 @@ var config = {
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
-                    options: {
-                        presets: [
-                            ["@babel/preset-env"],
-                            ["@babel/preset-react", {"runtime": "automatic"}],
-                        ],
-                    },
                 },
             },
             {
