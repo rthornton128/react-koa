09da1a8f5749dba449d9e636b14da6656b35f168
Rob Thornton <rthornton128@gmail.com>
2020-11-14 16:47:41 -0500

    Import bootstrap as a standard css file
    By including the minified version by default, Bootstrap can be used as
    normal by the app. Compiling the source may have some advantages but
    the headache is not worth it at this time.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./foundation/Header.jsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export function App() {
+  return (
+    <div id="app" className="container">
+      <div className="row">
+        <Header />
+      </div>
+    </div>
+  );
+}
